f0c091543c1a68800b43c947c735625560e7f13b
Jayasree Neelapu <jayasree.n.reddy01@gmail.com>
2024-06-10 13:54:46 -0700

    task#2-Adding react props and using map() and arrow functions to create multiple notes components without repeatitive code.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((notesItem) => (
+        <Note title={notesItem.title} content={notesItem.content} />
+      ))}
       <Footer />
     </div>
   );
