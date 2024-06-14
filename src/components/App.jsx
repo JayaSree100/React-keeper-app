8fbed888de461da1c137e8be696d1bac4c2fd883
Jayasree Neelapu <jayasree.n.reddy01@gmail.com>
2024-06-14 16:01:45 -0700

    Adding functionality and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNotes(newNotes) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNotes];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) =>
+      prevNotes.filter((note, index) => {
+        return index !== id;
+      })
+    );
+  }
   return (
     <div>
       <Header />
-      {notes.map((notesItem) => (
-        <Note title={notesItem.title} content={notesItem.content} />
-      ))}
+      <CreateArea onAdd={addNotes} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            title={note.title}
+            content={note.content}
+            key={index}
+            id={index}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
